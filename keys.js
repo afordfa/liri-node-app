ed9d0ebfd93222b5b8b86d3f5152ed6837730c7a
afordfa <anitamford@gmail.com>
2017-04-01 13:44:55 -0500

    homework added

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '60PLqiUbUtq7WceNTgWFxC9sg',
+  consumer_secret: 'uf98DvhMd7M3ul2I2NALsPIvqrpi0W6rIPzO5RnXscp1F4Se7P',
+  access_token_key: '86007956-cFE4oKLc53ysZheJC1Vwg8NwNYWhaUkxxRgOeAeoJ',
+  access_token_secret: 'PETRv49hyHjNIdVq5asQJPbOxFHywOZDRsqb8PYEOoOXn',
+}
